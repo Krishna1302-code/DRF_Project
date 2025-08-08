295e4c69df509384559da4b93f5a8609a163d82b
Krishna <krishna.joshi@simformsolutions.com>
2025-08-08 16:02:13 +0530

    [ADD] added models

--- RunwayREST/store/admin.py
+++ RunwayREST/store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from  .models import Product,Category,Order,OrderItem 
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','description','price','stock','image']
+
+admin.site.register(Product,ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category,CategoryAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_id','user', 'created_at', 'status']
+
+admin.site.register(Order,OrderAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(OrderItem,OrderItemAdmin)
