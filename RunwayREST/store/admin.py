fb98c65ef007084d1acd2dde402737d6c7916e54
Krishna <krishna.joshi@simformsolutions.com>
2025-08-12 19:46:47 +0530

    [ADD] Added more models and fixed some relationships

--- RunwayREST/store/admin.py
+++ RunwayREST/store/admin.py
@@ -1,22 +1,49 @@
 from django.contrib import admin
-from  .models import Product,Category,Order,OrderItem 
+from  .models import Product, Category, Order, OrderItem, Product_image, Address, Cart, Cart_item, Review
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','description','price','stock','image']
+    list_display = ['name','description','price','stock','gender','category']
 
-admin.site.register(Product,ProductAdmin)
+@admin.register(Product_image)
+class Product_imageAdmin(admin.ModelAdmin):
+    list_display = ['product']
 
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display =['user','street','city','state','country','pincode']
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
 
-admin.site.register(Category,CategoryAdmin)
+class ProductInline(admin.TabularInline):
+    model = Cart.products.through  # Access the M2M table
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+
+
+
+@admin.register(Cart_item)
+class Cart_itemAdmin(admin.ModelAdmin):
+   list_display = ['cart', 'product', 'quantity']
+   list_filter = ['cart', 'product']
+   search_fields = ['product__name', 'cart__user__username']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user','product', 'rating', 'comments','created_at']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['order_id','user', 'created_at', 'status']
+    list_display = ('id', 'customer', 'status', 'created_at', 'show_items')
 
-admin.site.register(Order,OrderAdmin)
+    def show_items(self, obj):
+        return ", ".join([item.name for item in obj.items.all()])
+    show_items.short_description = "Items"
 
+
+@admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(OrderItem,OrderItemAdmin)
+    list_display = ['order','product','quantity']
--- RunwayREST/store/models.py
+++ RunwayREST/store/models.py
@@ -1,17 +1,32 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-import uuid
+from django.db import models # Base ORM tools
+from django.contrib.auth.models import AbstractUser  # Base user model for authentication
+import uuid  # For unique IDs
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 
 
 class User(AbstractUser):
-    pass 
+    full_name = models.CharField(max_length=100,default='abc')
+    email = models.EmailField()
+    phone = PhoneNumberField(unique = True ,blank = False,null=True)
+
+    def __str__(self):
+        return self.full_name
 
 class Product(models.Model):
-    name = models.TextField
-    description = models.CharField(max_length=500)
+
+    GENDER_CHOICES=[
+        ('F','FEMALE'),
+        ('M','MALE'),
+    ]
+
+    name = models.CharField(max_length=100,null=True)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10 , decimal_places=2)
     stock = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='product/', blank=True , null=True )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=False,default='F')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)  
 
     @property
     def in_stock(self):
@@ -19,33 +34,69 @@ class Product(models.Model):
    
     def __str__(self):
         return self.name
-    
+
+class Product_image(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='images')
+    image_url = models.ImageField(upload_to='product/')
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='addresses')
+    street = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=50)
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
-class Order(models.Model):
-    class Statuschoices(models.TextChoices):    
-        PENDING = 'Pending'
-        CONFIRMED = 'Confirmed'
-        CANCELLED = 'Cancelled'
-
-    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(
-        max_length=10, 
-        choices=Statuschoices.choices, 
-        default=Statuschoices.PENDING
-    )
-    products = models.ManyToManyField(Product,through="OrderItem",related_name='orders')
-
-
     def __str__(self):
+        return self.name
+    
+class Cart(models.Model):#this is intermediate table between cart and product for user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through="Cart_Item")
+
+
+
+class Cart_item(models.Model):#this is through table cuz of many to many relationship 
+    quantity = models.PositiveIntegerField(default=1)
+    cart = models.ForeignKey(Cart ,on_delete=models.CASCADE,related_name= 'cart_item')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="cart_items")
+    #ManyToManyRel is not meant to be used directly in models,
+    #For a through table (Cart_Item), you should be using ForeignKey for both sides, not a ManyToManyField.
+
+class Review(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name= 'review')
+    rating = models.IntegerField()
+    comments = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class Order(models.Model):
+     STATUS_CHOICE=[  
+        ('PENDING','Pending'),
+        ('CONFIRMED','Confirmed'),
+        ('CANCELLED', 'Cancelled'),
+    ] 
+     order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)#need to understand this like if django already providing it then why
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     created_at = models.DateTimeField(auto_now_add=True)
+     price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+     subtotal = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+     total = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+
+     status = models.CharField(max_length=10,choices = STATUS_CHOICE, blank=False)
+     products = models.ManyToManyField(Product,through="OrderItem",related_name='order')
+
+
+     def __str__(self):
         return f"Order {self.order_id} by {self.user.username}"
     
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+
     quantity = models.PositiveIntegerField()
 
     @property
